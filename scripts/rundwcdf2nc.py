4c5b3b723b90dbf00d3d88202186994dc812a89d
Dan Nowacki <dnowacki@usgs.gov>
2017-09-13 15:05:02 -0400

    Rename scripts

--- scripts/rundwcdf2nc.py
+++ scripts/rundwcdf2nc.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser(description='Convert raw RBR d|wave .cdf format
 parser.add_argument('cdfname', help='raw .CDF filename')
 parser.add_argument('gatts', help='path to global attributes file (gatts formatted)')
 parser.add_argument('config', help='path to ancillary config file (YAML formatted)')
-parser.add_argument('--p_1ac', help='path to cdf file containing atmospherically corrected pressure data of same length as pressure data')
+parser.add_argument('--atmpres', help='path to cdf file containing atmopsheric pressure data')
 
 args = parser.parse_args()
 
@@ -25,8 +25,8 @@ config = yaml.safe_load(open(args.config))
 for k in config:
     metadata[k] = config[k]
 
-if args.p_1ac:
-    press_ac = aqdlib.load_press_ac('press_ac.cdf', ['p_1ac'])
-    ds = rsklib.cdf_to_nc(metadata, p_1ac=press_ac['p_1ac'])
+if args.atmpres:
+    # press_ac = aqdlib.load_press_ac('press_ac.cdf', ['p_1ac'])
+    ds = rsklib.cdf_to_nc(metadata, atmpres=args.atmpres)
 else:
     ds = rsklib.cdf_to_nc(metadata)
